054d19a784dbe11d929ccbb10c7845e51e9bef6f
Jarosław Szepelak <szepelakjaroslaw@gmail.com>
2024-12-07 15:57:00 +0100

    Dodanie walidacji

--- ShopWithMe/ShopWithMe/Models/Products/Admin/ProductFormModel.cs
+++ ShopWithMe/ShopWithMe/Models/Products/Admin/ProductFormModel.cs
@@ -1,11 +1,18 @@
-﻿namespace ShopWithMe.Models.Products.Admin
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopWithMe.Models.Products.Admin
 {
     public class ProductFormModel
     {
         public long Id { get; set; }
+        [Required(ErrorMessage = "Nazwa kategorii jest wymagana.")]
         public string Name { get; set; }
         public string Lead { get; set; }
         public string Description { get; set; }
+        [Required(ErrorMessage = "Cena jest wymagana.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Proszę podać dodatnią cenę.")]
+        [RegularExpression("[0-9]?[0-9]?[0-9?]?[0-9]?[0-9]?[0-9]((,|.)[0-9][0-9]?)?", ErrorMessage = "Podano nieprawidłową cenę.")]
         public decimal Price { get; set; }
         public long? CategoryId { get; set; }
     }
