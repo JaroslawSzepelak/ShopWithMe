9893661523f03fdad1f691d2b878bc12ca2cdd89
Jarosław Szepelak <szepelakjaroslaw@gmail.com>
2025-01-02 15:07:30 +0100

    Dodanie obsługi kont użytkowników

---
+++ ShopWithMe/ShopWithMe/Models/IdentityModels/Accounts/Admin/UserListModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopWithMe.Models.IdentityModels.Accounts.Admin
+{
+    public class UserListModel
+    {
+        public string Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
